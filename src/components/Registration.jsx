ab5c5c8421fe4194ccea302716561bd4a4864f5c
aayush <ayushsahu3010@gmail.com>
2024-08-14 16:52:57 +0530

    update again

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -57,6 +57,15 @@ function Registration() {
               className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
             />
           </div>
+          <div className="mb-5">
+            <input
+              type="text"
+              name="collegename"
+              id="collegename"
+              placeholder="Enter your college name"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
 
           <div className="mb-5">
           <label className="mb-3 block text-2xl font-medium text-[#07074D] leading-loose">
