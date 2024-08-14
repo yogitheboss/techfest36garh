b86e33bd458d5f98182812f8d677cf0a3eaab4f6
Yogitheboss <yograjrjpt123@gmail.com>
2024-08-14 14:53:40 +0530

    integrating events

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { useState } from 'react'
-import Events from './components/events'
-import './App.css'
-import HackathonPage from './components/HackathonPage'
-import {  Routes, Route} from 'react-router-dom'
+import "./App.css";
+import HackathonPage from "./components/HackathonPage";
+import { Routes, Route } from "react-router-dom";
+import Main from "./MainPage";
 
 function App() {
- 
-
   return (
-    < >
-    
-    <Routes>
-      <Route path='/' element={<Events/>}/>
-   <Route path='/hackathon' element={<HackathonPage/>}/>
-    </Routes>
-      
-
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/hackathon" element={<HackathonPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
